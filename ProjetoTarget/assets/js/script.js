7d7700163c0213109113bbd67e768492dd972c8a
thiago morgado <thiago_morgado@hotmail.com>
2024-10-05 01:17:43 -0300

    reafacot javascript

--- ProjetoTarget/assets/js/script.js
+++ ProjetoTarget/assets/js/script.js
@@ -1,9 +1,12 @@
-document.getElementById('menu-toggle').addEventListener('click', function () {
+document.addEventListener('DOMContentLoaded', function () {
   const menu = document.getElementById('menu-mobile-item');
-  if (menu.style.display === 'none' || menu.style.display === '') {
-    menu.style.display = 'block'; // Mostra o menu
-  } else {
-    menu.style.display = 'none'; // Esconde o menu
-  }
-});
+  menu.style.display = 'none'; // Inicia o menu fechado
 
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+    if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block'; // Mostra o menu
+    } else {
+      menu.style.display = 'none'; // Esconde o menu
+    }
+  });
+});
